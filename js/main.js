34d6403044964c26645bac2f6591036c14d06e20
Rus LAN <okeyff@gmail.com>
2020-05-07 17:12:51 +0300

    Desktop Icons, StartUp / SubMenu

--- js/main.js
+++ js/main.js
@@ -4,9 +4,24 @@ document.querySelector('.taskbar-tasks_btn').onclick = function () {
 
 document.querySelector('.open-startup-menu').onclick = function () {
     let startMenu = document.querySelector('.startup-menu');
-    if (startMenu.style.display === "flex") {
-        startMenu.style.display = "none";
+    if (startMenu.style.display === 'flex') {
+        startMenu.style.display = 'none';
     } else {
-        startMenu.style.display = "flex";
+        startMenu.style.display = 'flex';
     }
 }
+
+function showSubMenu() {
+    let subMenu = document.querySelector('.startup-sub-menu');
+    subMenu.style.display = 'block';
+}
+
+function hideSubMenu() {
+    let subMenu = document.querySelector('.startup-sub-menu');
+    subMenu.style.display = 'none';
+}
+
+// document.querySelector('.desktop-icons_item').onclick = function () {
+//     document.querySelector('.desktop-icon__text').style.backgroundColor = 'navy';
+// }
+
