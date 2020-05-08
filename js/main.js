022716060ec089b31b1f83efab4f36d0404720e0
Rus LAN <okeyff@gmail.com>
2020-05-08 18:06:11 +0300

    controlPanel, closeBtn js, window

--- js/main.js
+++ js/main.js
@@ -21,7 +21,34 @@ function hideSubMenu() {
     subMenu.style.display = 'none';
 }
 
-// document.querySelector('.desktop-icons_item').onclick = function () {
+// document.querySelector('.desktop-icons_item').ondblclick = function () {
+//     let folderName = document.querySelector;
+//     let windowFolderName = document.querySelector;
+//     if (windowFolderName === 'folderName') {
+//         return;
+//     } else {
+//         folderName = document.querySelector('.desktop-icon__text');
+//         windowFolderName = document.querySelector('.window-title_text').innerHTML += 'folderName ';
+//     }
+// }
+
+document.getElementById('control-panel').onclick = function () {
+    document.querySelector('.control-panel').style.display = 'block';
+}
+
+document.querySelector('.window-close-btn').onclick = function () {
+    document.querySelector('.window').style.display = 'none';
+}
+
+
+
+// document.querySelector('.desktop-icons_item').onclick = function() {
 //     document.querySelector('.desktop-icon__text').style.backgroundColor = 'navy';
 // }
 
+// var btn = document.querySelector('.desktop-icons_item');
+// btn.addEventListener('click', foo);
+
+// function foo() {
+//     document.querySelector('.desktop-icon__text').style.backgroundColor = 'navy';
+// }
